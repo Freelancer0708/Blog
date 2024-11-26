e490143c7f28ae78279b65df36771b94cce60278
Freelancer0708 <freelancernoaccont@gmail.com>
2024-11-27 08:54:19 +0900

    add login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +32,7 @@ export default function RootLayout({
           <Link href="/">Home</Link>
           <Link href="/blog">Blog</Link>
         </header>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+// src/app/login/page.tsx
+"use client";
+import styles from "./login.module.css";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { auth } from "../../firebase/config";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push("/blog/new");
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className={styles.login}>
+      <main className={styles.main}>
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </main>
+    </div>
+  );
+}
